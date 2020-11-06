c9f9355807139ed803f5f1e058358d42c7665f9d
montipallarols <montaga.pallarols@gmail.com>
2020-11-06 13:50:37 +0100

    Set up routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { Route, Switch } from "react-router";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello world!</h1>
+      <Switch>
+      <Route exact path = "/" component={HomePage}></Route>
+      <Route exact path = "/product/:id" component={ProductPage}></Route>
+      <Route exact path = "/order" component={CartPage}></Route>
+      </Switch>
     </div>
   );
 }
